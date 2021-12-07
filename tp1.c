f72b6e8382c46cfd14bb60eb3eee81e2bc7adeb4
Fabien Darrigrand <71562075+FabienDarr@users.noreply.github.com>
2021-12-07 16:40:22 +0100

    Add files via upload

---
+++ tp1.c
@@ -0,0 +1,26 @@
+/*!
+\file tp1.c
+\author Fabien Darrigrand <darrigrand@cy-tech.fr>
+\version 1
+\date 12 novembre 2021
+\brief Premier TP (2021) avec la programmation C
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*!
+\fn int main (int argc, char** argv)
+\author Fabien Darrigrand <darrigrand@cy-tech.fr>
+\version 1
+\date 12 novembre 2021
+\brief Bonjour tout le monde
+\param argc nombre d'arguments en entree
+\param argv valeur des arguments en entree
+\return 0 si tout c'est bien passe
+*/
+
+int main (int argc, char** argv){
+    printf("Bonjour tout le monde\n");
+    return (0);
+}
